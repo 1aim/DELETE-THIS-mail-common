e46201e14e87c172f65da619acd9cc274791ae7f
Philipp Korber <philippkorber@gmail.com>
2018-01-16 16:49:18 +0100

    chore: turned this into only mail-codec-core
    i.e. all other parts where removed

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,37 @@
 #![recursion_limit="128"]
-
-#[cfg_attr(test, macro_use)]
-extern crate mail_codec_core;
-
 #[macro_use]
-extern crate log;
+extern crate error_chain;
+#[macro_use]
+extern crate nom;
+extern crate owning_ref;
+extern crate chrono;
 extern crate mime;
+extern crate total_order_multi_map;
+extern crate soft_ascii_string;
+extern crate base64;
 extern crate quoted_printable;
 extern crate idna;
-extern crate chrono;
-extern crate futures;
-extern crate serde;
-extern crate base64;
-extern crate rand;
+extern crate quoted_string;
+extern crate media_type_impl_utils;
 extern crate percent_encoding;
-extern crate soft_ascii_string;
-extern crate total_order_multi_map;
-extern crate tree_magic;
-
-#[cfg_attr(test, macro_use)]
 extern crate vec1;
-
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+
+
+
+//NOTE: this would be worth it's own independent crate for utility macros
 #[macro_use]
-extern crate scoped_tls;
-
-
-#[cfg(feature="default_impl_cpupool")]
-extern crate futures_cpupool;
-
+pub mod macros;
 
 #[macro_use]
-pub mod external;
+pub mod utils;
+pub mod error;
+pub mod grammar;
 #[cfg_attr(test, macro_use)]
 pub mod codec;
 pub mod data;
-pub mod components;
-#[macro_use]
 pub mod headers;
-pub mod mail;
-pub mod composition;
-
-#[cfg(feature="default_impl_any")]
-pub mod default_impl;
-
-pub mod mail_builder_prelude {
-    pub type Encoder = ::codec::Encoder<::mail::Resource>;
-    pub use error::*;
-    pub use grammar::MailType;
-    pub use codec::{EncodableInHeader, Encodable, EncodeHandle};
-    pub use data::FromInput;
-    pub use headers::*;
-    pub use components::*;
-    pub use mail::Builder;
-    pub use mail::mime::MultipartMime;
-}
-
-
-pub mod resource_prelude {
-    pub use utils::FileBuffer;
-    pub use utils::FileMeta;
-    pub use mail::{ Resource, ResourceSpec };
-    pub use composition::{ Embedding, Attachment, EmbeddingWithCID };
-}
-
-pub mod composition_prelude {
-    pub type Encoder = ::codec::Encoder<::mail::Resource>;
-    pub use error::*;
-    pub use grammar::MailType;
-    pub use data::FromInput;
-    pub use components::{
-        Mailbox,
-        Email,
-        TransferEncoding
-    };
-    pub use codec::{
-        EncodableInHeader,
-        EncodeHandle,
-        Encodable
-    };
-    pub use composition::{
-        Compositor,
-        NameComposer,
-        MailSendContext,
-    };
-}
-
-pub mod template_engine_prelude {
-    pub type StdError = ::std::error::Error;
-    pub type StdResult<R,E> = ::std::result::Result<R,E>;
-    pub use serde::Serialize;
-
-    pub use vec1::Vec1;
-    pub use mail::{
-        Resource
-    };
-    pub use composition::{
-        Template, TemplateEngine,
-        Context,
-        Attachment, Embedding
-    };
- }
+pub mod components;
